e7c9a2f38f050211c701c9b504ed5a3c3fcf44e9
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-06-10 18:55:42 +0800

    Update run_analysis.py

--- run_analysis.py
+++ run_analysis.py
@@ -6,6 +6,7 @@ import corner
 from pathlib import Path
 import matplotlib.pyplot as plt
 import h5py
+import os
 
 
 
@@ -68,14 +69,14 @@ def plotRunAnalysis(summary, event, output_dir="output"):
     mkdir(output_dir + "/posterior_analysis")
     plt.savefig(output_dir + "/posterior_analysis/"+event+".jpeg")
 
-def plotLikelihood(summary, event):
+def plotLikelihood(summary, event, output_dir="output"):
     chains, log_prob, local_accs, global_accs, loss_vals = summary.values()
     log_prob = np.array(log_prob)
     
     # Create a figure and axis
     fig, ax = plt.subplots()
 
-    fig = plt.plot(log_prob[0], linewidth=0.5)
+    fig = plt.plot(log_prob[0], linewidth=1.0)
     plt.ylim(bottom=-20)
     mkdir(output_dir + "/likelihood_single_line")
     plt.savefig(output_dir + "/likelihood_single_line/"+event+".jpeg")
@@ -91,3 +92,62 @@ def plotLikelihood(summary, event):
 def mkdir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+"""
+['final_spin', 'spin_2y', 'final_mass_source', 'spin_1y', 'cos_tilt_2', 'mass_1_source', 
+'viewing_angle', 'spin_2x', 'inverted_mass_ratio', 'phi_2', 'chi_p', 'chirp_mass', 'chirp_mass_source', 
+'total_mass', 'redshift', 'luminosity_distance', 'theta_jn', 'chi_eff', 'a_1', 'cos_iota', 
+'radiated_energy', 'cos_theta_jn', 'total_mass_source', 'phi_jl', 'mass_2', 'ra', 'final_mass', 
+'spin_1x', 'log_likelihood', 'tilt_2', 'tilt_1', 'psi', 'dec', 'symmetric_mass_ratio', 'mass_2_source', 
+'iota', 'psi_J', 'cos_tilt_1', 'phi_12', 'mass_ratio', 'comoving_distance', 'phase', 'beta', 'chi_p_2spin', 
+'phi_1', 'a_2', 'spin_1z', 'peak_luminosity', 'spin_2z', 'mass_1', 'tilt_1_infinity_only_prec_avg', 
+'tilt_2_infinity_only_prec_avg', 'spin_1z_infinity_only_prec_avg', 'spin_2z_infinity_only_prec_avg', 
+'chi_eff_infinity_only_prec_avg', 'chi_p_infinity_only_prec_avg', 'cos_tilt_1_infinity_only_prec_avg', 
+'cos_tilt_2_infinity_only_prec_avg']
+"""
+
+def compare_plot(event_name):
+    bilby_posterior_dir = "data/IGWN-GWTC3p0-v1-" + event_name + "_PEDataRelease_mixed_cosmo.h5"
+    jim_posterior_dir = "output/posterior_samples/" + event_name + ".h5"
+
+    
+
+    file = h5py.File(jim_posterior_dir, 'r')
+    jim_posterior = np.array(file['posterior'])
+    sample_point_filter = np.random.choice(jim_posterior.shape[0], size=5000, replace=True)
+    
+    jim_posterior = np.array([jim_posterior[:,0], jim_posterior[:,1], jim_posterior[:,2], jim_posterior[:,3], jim_posterior[:,4], jim_posterior[:,5], jim_posterior[:,6], jim_posterior[:,7]]).T
+    jim_posterior = jim_posterior[sample_point_filter]
+    file.close()
+
+    file = h5py.File(bilby_posterior_dir, 'r')
+    eta= np.array(file['C01:Mixed']['posterior_samples']['symmetric_mass_ratio'])
+    sample_point_filter = np.random.choice(eta.shape[0], size=5000, replace=True)
+    spin_1x = np.array(file['C01:Mixed']['posterior_samples']['spin_1x'])
+    spin_1y = np.array(file['C01:Mixed']['posterior_samples']['spin_1y'])
+    spin_1z = np.array(file['C01:Mixed']['posterior_samples']['spin_1z'])
+    spin_2x = np.array(file['C01:Mixed']['posterior_samples']['spin_2x'])
+    spin_2y = np.array(file['C01:Mixed']['posterior_samples']['spin_2y'])
+    spin_2z = np.array(file['C01:Mixed']['posterior_samples']['spin_2z'])
+    # spin_1_theta = np.arccos(spin_1z)
+    # spin_2_theta = np.arccos(spin_2z)
+    # spin_1_phi = np.arctan2(spin_1y, spin_1x)
+    # spin_2_phi = np.arctan2(spin_2y, spin_2x)
+    # spin_1_r = np.sqrt(spin_1x**2 + spin_1y**2 + spin_1z**2)
+    # spin_2_r = np.sqrt(spin_2x**2 + spin_2y**2 + spin_2z**2)
+    bilby_posterior = np.array([np.array(file['C01:Mixed']['posterior_samples']['chirp_mass']), eta, spin_1x, spin_1y, spin_1z, spin_2x, spin_2y, spin_2z])
+    bilby_posterior = bilby_posterior.T
+    bilby_posterior = bilby_posterior[sample_point_filter]
+    file.close()
+
+    labels = ["M_c", "eta", "spin1x", "spin1y", "spin1z", "spin2x", "spin2y", "spin2z"]
+        
+
+    fig = corner.corner(jim_posterior, labels=labels, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True, color = 'red')
+    corner.corner(bilby_posterior, labels=labels, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True, color = 'blue', fig=fig)
+
+    plt.savefig("compare_plot/"+event_name+".jpeg")
+    
+
+def sampling(arr):
+    return np.random.choice(arr, size=5000, replace=False)
