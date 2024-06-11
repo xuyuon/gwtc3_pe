7176eb0f4dace72e388230b4b33749b66f75decc
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-06-11 15:38:24 +0800

    Create all_events_pe.py

---
+++ all_events_pe.py
@@ -0,0 +1,8 @@
+from single_event_pe import runSingleEventPE
+
+from gwosc.datasets import find_datasets, event_gps
+
+
+
+
+gwtc3 = datasets.find_datasets(type='events', catalog='GWTC-3-confident')
