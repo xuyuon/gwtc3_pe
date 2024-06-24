0bbdb7f16ecb3dea5aa37a34d80e931f2e1925e2
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-06-24 12:44:12 +0800

    Revert "Uniform component mass prior"

--- gen_summary.py
+++ gen_summary.py
@@ -1,4 +1,4 @@
-from parameter_estimation.save import saveSummary
+from run_analysis import output_summary
 from os import listdir
 from os.path import isfile, join
 
@@ -8,7 +8,7 @@ from gwosc import datasets
 path = "output/posterior_samples"
 files = [f[:-3] for f in listdir(path) if isfile(join(path, f))]
 
-saveSummary(files)
+output_summary(files)
 
 gwtc3 = datasets.find_datasets(type='events', catalog='GWTC-3-confident')
 missing_events = set(gwtc3) - set(files)
