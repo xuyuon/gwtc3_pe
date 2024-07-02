b0f326a51c7f36c9257748a991eed15125fb7c08
Yu On (Ben) Xu <yxu10@workergpu016.cm.cluster>
2024-07-02 06:53:20 -0400

    Updated plotting.py

--- parameter_estimation/plotting.py
+++ parameter_estimation/plotting.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import h5py
 
 from .utilities import mkdir
-
+from .fetch import getBilbyPosterior
+from .save import getPosterior
 
 """
 Available params:
@@ -29,7 +30,7 @@ def plotPosterior(result, event, output_dir="output", labels=["M_c", "eta", "s_1
     Plot the posterior samples in a corner plot
     """
     
-    samples = np.array(list(result.values())).reshape(15, -1) # flatten the array
+    samples = np.array(list(result.values())).reshape(int(len(labels)), -1) # flatten the array
     transposed_array = samples.T # transpose the array
     figure = corner.corner(transposed_array, labels=labels, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True)
     mkdir(output_dir + "/posterior_plot")
@@ -107,74 +108,11 @@ def plotCompare(event_name, params, output_dir="compare_plot"):
     
     params: A list of params to be included in the plot
     """
-    bilby_posterior_dir = "data/IGWN-GWTC3p0-v1-" + event_name[:-3] + "_PEDataRelease_mixed_cosmo.h5"
-    jim_posterior_dir = "output/posterior_samples/" + event_name + ".h5"    
-
-    file = h5py.File(jim_posterior_dir, 'r')
-    jim_posterior = np.array(file['posterior'])
-    file.close()
-    
-    file = h5py.File(bilby_posterior_dir, 'r')
-    
     jim_params = []
     bilby_params = []
-    if "chirp_mass" in params:
-        jim_params.append(jim_posterior[:,0])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['chirp_mass']))
-    
-    if "eta" in params:
-        jim_params.append(jim_posterior[:,1])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['symmetric_mass_ratio']))
-    
-    if "spin1x" in params:
-        jim_params.append(jim_posterior[:,2])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['spin_1x']))
-    
-    if "spin1y" in params:
-        jim_params.append(jim_posterior[:,3])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['spin_1y']))
-    
-    if "spin1z" in params:
-        jim_params.append(jim_posterior[:,4])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['spin_1z']))
-    
-    if "spin2x" in params:
-        jim_params.append(jim_posterior[:,5])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['spin_2x']))
-    
-    if "spin2y" in params:
-        jim_params.append(jim_posterior[:,6])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['spin_2y']))
-    
-    if "spin2z" in params:
-        jim_params.append(jim_posterior[:,7])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['spin_2z']))
-    
-    if "luminosity_distance" in params:
-        jim_params.append(jim_posterior[:,8])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['luminosity_distance']))
-        
-    if "phase" in params:
-        jim_params.append(jim_posterior[:,10])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['phase']))
-        
-    if "iota" in params:
-        jim_params.append(jim_posterior[:,11])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['iota']))
-    
-    if "psi" in params:
-        jim_params.append(jim_posterior[:,12])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['psi']))
-    
-    if "ra" in params:
-        jim_params.append(jim_posterior[:,13])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['ra']))
-        
-    if "dec" in params:
-        jim_params.append(jim_posterior[:,14])
-        bilby_params.append(np.array(file['C01:Mixed']['posterior_samples']['dec']))
-    
-    file.close()
+    for param in params:
+        jim_params.append(getPosterior(event_name, param))
+        bilby_params.append(getBilbyPosterior(event_name, param))
         
     jim_params = np.array(jim_params).T
     sample_point_filter = np.random.choice(jim_params.shape[0], size=5000, replace=True)
@@ -183,17 +121,9 @@ def plotCompare(event_name, params, output_dir="compare_plot"):
     bilby_params = np.array(bilby_params).T
     sample_point_filter = np.random.choice(bilby_params.shape[0], size=5000, replace=True)
     bilby_params = bilby_params[sample_point_filter]
-    # spin_1_theta = np.arccos(spin_1z)
-    # spin_2_theta = np.arccos(spin_2z)
-    # spin_1_phi = np.arctan2(spin_1y, spin_1x)
-    # spin_2_phi = np.arctan2(spin_2y, spin_2x)
-    # spin_1_r = np.sqrt(spin_1x**2 + spin_1y**2 + spin_1z**2)
-    # spin_2_r = np.sqrt(spin_2x**2 + spin_2y**2 + spin_2z**2)
 
-    labels = params
-
-    fig = corner.corner(jim_params, labels=labels, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True, color = 'red')
-    corner.corner(bilby_params, labels=labels, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True, color = 'blue', fig=fig)
+    fig = corner.corner(jim_params, labels=params, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True, color = 'red')
+    corner.corner(bilby_params, labels=params, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True, color = 'blue', fig=fig)
 
     plt.savefig(output_dir+"/"+event_name+".jpeg")
     
@@ -202,11 +132,12 @@ def plotIntrinsicParamsComparison(event_name, output_dir="compare_plot"):
     """
     To compare the intrinsic parameters of the posterior samples from Jim and Bilby
     """
-    plotCompare(event_name, ["chirp_mass", "eta", "spin1x", "spin1y", "spin1z", "spin2x", "spin2y", "spin2z"], output_dir)
-  
+    # plotCompare(event_name, ["chirp_mass", "eta", "spin1x", "spin1y", "spin1z", "spin2x", "spin2y", "spin2z"], output_dir)
+    plotCompare(event_name, ["M_c", "eta", "a_1", "a_2", "phi_12", "phi_jl", "tilt_1", "tilt_2"], output_dir)
 
 def plotExtrinsicParamsComparison(event_name, output_dir="compare_plot"):
     """
     To compare the extrinsic parameters of the posterior samples from Jim and Bilby
     """
-    plotCompare(event_name, ["luminosity_distance", "phase", "iota", "psi", "ra", "dec"], output_dir)
+    # plotCompare(event_name, ["luminosity_distance", "phase", "iota", "psi", "ra", "dec"], output_dir)
+    plotCompare(event_name, ["d_L", "phase_c", "theta_jn", "psi", "ra", "dec"], output_dir)
