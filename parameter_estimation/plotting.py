354c7426107b535425b25b26107d8d6aae9b2b7e
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-07-02 19:03:24 +0800

    Update plotting.py

--- parameter_estimation/plotting.py
+++ parameter_estimation/plotting.py
@@ -9,19 +9,6 @@ from .utilities import mkdir
 from .fetch import getBilbyPosterior
 from .save import getPosterior
 
-"""
-Available params:
-['final_spin', 'spin_2y', 'final_mass_source', 'spin_1y', 'cos_tilt_2', 'mass_1_source', 
-'viewing_angle', 'spin_2x', 'inverted_mass_ratio', 'phi_2', 'chi_p', 'chirp_mass', 'chirp_mass_source', 
-'total_mass', 'redshift', 'luminosity_distance', 'theta_jn', 'chi_eff', 'a_1', 'cos_iota', 
-'radiated_energy', 'cos_theta_jn', 'total_mass_source', 'phi_jl', 'mass_2', 'ra', 'final_mass', 
-'spin_1x', 'log_likelihood', 'tilt_2', 'tilt_1', 'psi', 'dec', 'symmetric_mass_ratio', 'mass_2_source', 
-'iota', 'psi_J', 'cos_tilt_1', 'phi_12', 'mass_ratio', 'comoving_distance', 'phase', 'beta', 'chi_p_2spin', 
-'phi_1', 'a_2', 'spin_1z', 'peak_luminosity', 'spin_2z', 'mass_1', 'tilt_1_infinity_only_prec_avg', 
-'tilt_2_infinity_only_prec_avg', 'spin_1z_infinity_only_prec_avg', 'spin_2z_infinity_only_prec_avg', 
-'chi_eff_infinity_only_prec_avg', 'chi_p_infinity_only_prec_avg', 'cos_tilt_1_infinity_only_prec_avg', 
-'cos_tilt_2_infinity_only_prec_avg']
-"""
 
 ############################## Functions for Plotting Various Graphs ##############################
 
