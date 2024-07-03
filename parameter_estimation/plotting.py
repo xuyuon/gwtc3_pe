5a8b138962c5ef9f6a2aa6aaa110ba6565a8af5c
Yu On (Ben) Xu <yxu10@rustyamd1.cm.cluster>
2024-07-03 04:12:06 -0400

    plotting.py

--- parameter_estimation/plotting.py
+++ parameter_estimation/plotting.py
@@ -119,12 +119,12 @@ def plotIntrinsicParamsComparison(event_name, output_dir="compare_plot"):
     """
     To compare the intrinsic parameters of the posterior samples from Jim and Bilby
     """
-    # plotCompare(event_name, ["chirp_mass", "eta", "spin1x", "spin1y", "spin1z", "spin2x", "spin2y", "spin2z"], output_dir)
-    plotCompare(event_name, ["M_c", "eta", "a_1", "a_2", "phi_12", "phi_jl", "tilt_1", "tilt_2"], output_dir)
+    plotCompare(event_name, ["M_c", "eta", "s1_x", "s1_y", "s1_z", "s2_x", "s2_y", "s2_z"], output_dir)
+    # plotCompare(event_name, ["M_c", "eta", "a_1", "a_2", "phi_12", "phi_jl", "tilt_1", "tilt_2"], output_dir)
 
 def plotExtrinsicParamsComparison(event_name, output_dir="compare_plot"):
     """
     To compare the extrinsic parameters of the posterior samples from Jim and Bilby
     """
-    # plotCompare(event_name, ["luminosity_distance", "phase", "iota", "psi", "ra", "dec"], output_dir)
-    plotCompare(event_name, ["d_L", "phase_c", "theta_jn", "psi", "ra", "dec"], output_dir)
+    plotCompare(event_name, ["d_L", "phase_c", "iota", "psi", "ra", "dec"], output_dir)
+    # plotCompare(event_name, ["d_L", "phase_c", "theta_jn", "psi", "ra", "dec"], output_dir)
